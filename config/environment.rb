7b8d2ceb1ca7034380b53db881ea9320bfae72d0
John Arevalo <john.arevalo11@gmail.com>
2017-01-06 01:56:58 -0500

    move mailer info to production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.gmail.com',
-  :domain         => 'mail.google.com',
-  :port           => 587,
-  :user_name      => ENV['user_name'],
-  :password       => ENV['password'],
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
